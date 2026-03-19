f270ecebdf85373c4357aa46e7f8d299c9f9dab1
Funasitien 🌥 <hello@funa.dev>
2026-03-19 10:40:01 +0100

    Fix circular db

--- utils/init_database.py
+++ utils/init_database.py
@@ -3,7 +3,9 @@ def init_database_scheme(db):
         "id": "INT",
         "name": "TEXT",
         "email": "TEXT",
-        "org_id": ("FOREIGN KEY", "orgs(id)"),
+        # avoid circular foreign-key (orgs -> users and users -> orgs)
+        # store org relationship as an integer reference instead
+        "org_id": "INT",
     }
     db.init_table("users", users_fields, primary_key="id")
     
